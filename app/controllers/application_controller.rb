cbaeda433d55c0d64b5f80196ce16d69220bb44b
Alpha Daniel <daniel.nunez.nyc@gmail.com>
2018-03-30 19:30:27 +0000

    moved ace testing to SnippetsController preparing for init routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,23 +12,6 @@ class ApplicationController < Sinatra::Base
   get '/' do 
     erb :"index"
   end
-#======================ace testing========================= 
-  # this will end up being /snippets/new
-  get '/ace' do
-    erb :'ace'
-  end
-#---------------------------------------------------------- 
-  post '/ace' do 
-    Snippet.create(content: params[:content].gsub(",", "\n"))
-    
-    redirect '/show'
-  end
-#----------------------------------------------------------   
-  get '/show' do 
-    @snippet = Snippet.first
-    
-    erb :'show'
-  end
 #========================helpers=========================== 
 
 #========================================================== 
--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -1,2 +1,20 @@
 class SnippetsController < ApplicationController
+#======================ace testing========================= 
+  # this will end up being /snippets/new
+  get '/ace' do
+    erb :"snippets/ace"
+  end
+#---------------------------------------------------------- 
+  post '/ace' do 
+    Snippet.create(content: params[:content].gsub(",", "\n"))
+    
+    redirect '/show'
+  end
+#----------------------------------------------------------   
+  get '/show' do 
+    @snippet = Snippet.first
+    
+    erb :"snippets/show"
+  end
+#========================================================== 
 end
