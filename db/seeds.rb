b5cda4438eb5d06b9365d29e17190da9c1392dad
Alpha Daniel <daniel.nunez.nyc@gmail.com>
2018-05-06 19:51:48 +0000

    tweaked seeds.rb - preping for live demo create limit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,20 @@
 # USERS
-users = [
-  {username: 'guest', email: 'e@1.com', password: '12345'},
-  {username: 'dev', email: 'e@2.com', password: '12345'}
-] 
+# users = [
+#   {username: 'guest', email: 'e@1.com', password: '12345'},
+#   {username: 'dev', email: 'e@2.com', password: '12345'}
+# ] 
 
 # CREATE USERS
-users.each{|u| User.create(u)}
+User.create(username: 'dev', email: 'e@2.com', password: '12345')
 
 # CREATE/ASSOCIATE SNIPPETS, SESSION, & TAGS
 a = 0
-10.times do
+20.times do
   snippet = Snippet.create(title: "Dummy Snippet #{a+1}", content: "def something\n  \nend")
   snippet.editor_session = EditorSession.create(mode: "ace/mode/ruby")
   
-  b = 0
-  2.times do 
-    snippet.tags << Tag.new(name: "Dummy Tag #{b+1}")
-    b += 1
-  end
+  snippet.tags << Tag.new(name: "Dummy Tag #{a+1}")
   
-  User.second.snippets << snippet
+  User.first.snippets << snippet
   a += 1
 end
